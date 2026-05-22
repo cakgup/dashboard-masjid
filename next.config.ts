9ca22b4e876fa993132884aed1a4b3a6c658d3bf
Mukh. Gufron Ikhsan <mukh.gufron@ui.ac.id>
2026-05-22 23:19:42 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/dashboard-masjid', // Sesuaikan dengan nama repository
   assetPrefix: '/dashboard-masjid/',
-  trailingSlash: true,
   images: {
     unoptimized: true, // Wajib diaktifkan karena GitHub Pages tidak mendukung Next.js Image Optimization bawaan
   },
